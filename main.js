ba1ef6f79c735aa5467198c4c642a03bc168f6ff
mbronckers <mbronckers@icloud.com>
2017-05-17 12:42:10 -0400

    Updated draw functions and user tiles
    The drawPlayer() and drawMap() functions are now fully functional.
    Merged Player and CSS branch successfully. Resolved conflicts.

--- main.js
+++ main.js
@@ -18,42 +18,8 @@ Graphic variables
 var texture = new Image();
 texture.src = "Graphics/TexturePack.png"; 
 var playerImg = new Image();
-playerImg.src = "Graphics/player.png"// file path to external texture files
+playerImg.src = "Graphics/user.png"// file path to external texture files
 
-<<<<<<< HEAD
-var user = new Image();
-user.src = "Graphics/user.png";             // file path to tiles for the player
-
-var grass = 12;								// the value of element in array [non-fighting grass]
-var grassX = 0;								// the x start location of grass tile in texture file --> meant for clipping
-var grassY = 3;                             // the y start location of grass tile in texture file --> meant for clipping
-
-var tree = 0;
-var treeX = 3;
-var treeY = 3;
-
-var healthCenter = 49;
-var healthCenterX;
-var healthCenterY;
-
-var map = {cols: 16, rows: 16, tileSize: 16,
-    tiles: [
-        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-=======
 var map = {cols: 16, rows: 16, tileSize: 16,
     tiles: [
         [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
@@ -73,21 +39,17 @@ var map = {cols: 16, rows: 16, tileSize: 16,
         [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
         [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
 
->>>>>>> CSS
     ],
     getTile: function(row, col) {
         return this.tiles[row][col];
     }
 };
 
-var player = {x: 200, y: 200};
-
 window.onload = function() {
     canvas = document.getElementById("game");
     ctx = canvas.getContext("2d");
     
-    
-    
+
     setInterval(function() {
         canvas.width = canvas.width;
         drawMap();
@@ -106,23 +68,7 @@ function drawMap() {
             var tile = map.getTile(r, c);
             
             if (tile != -1) { // a value of -1 in the map array means empty tile
-<<<<<<< HEAD
-                
-                if (tile == grass) {            // a case for each type of tile in array --> grass tile
-                    ctx.drawImage(
-                        texture,            // image file source
-                        (grassX) * map.tileSize, // the x coordinate of the image file to clip --> texture file tile's x position
-                        (grassY * map.tileSize), // the y coordinate of the image file to clip --> texture file tile's y position
-                        map.tileSize, 		// the width of the clipped image --> texture file tile's width
-                        map.tileSize,		// the height of the clipped image --> texture file tile's height
-                        c * (map.tileSize), 	// x position of tile on canvas
-                        r * (map.tileSize),	// y position of tile on canvas
-                        (map.tileSize),		// tile width
-                        (map.tileSize)		// tile height
-                    );
-                }
-            
-=======
+
                     ctx.drawImage(
                         texture,            // image file source
                         0, // the x coordinate of the image file to clip --> texture file tile's x position
@@ -134,49 +80,28 @@ function drawMap() {
                         map.tileSize * 2,		// tile width
                         map.tileSize * 2		// tile height
                     );
-
->>>>>>> CSS
             }
         }
     }
 }
 
 function drawPlayer() {
-<<<<<<< HEAD
         ctx.drawImage(
-            user // image file source
+            playerImg, // image file source
             0, 
             player.orientation * player.tileSize,
             player.tileSize,
             player.tileSize,
-            player.x * map.tileSize,
-            player.y * map.tileSize,
-            map.tileSize,
-            map.tileSize
+            player.x * player.tileSize,
+            player.y * player.tileSize,
+            map.tileSize * 2,
+            map.tileSize * 2
             );
-}
-
-/* --------------------------------------
-Keyboard Listener
-----------------------------------------*/
-=======
-    ctx.drawImage(
-        playerImg,
-        0,
-        0,
-        map.tileSize,
-        map.tileSize,
-        player.x * 2,
-        player.y * 2,
-        map.tileSize * 2,
-        map.tileSize * 2
-    );
 }
 
 //
 // Keyboard listener
 //
->>>>>>> CSS
 
 var KEY = {W: 87, A: 65, S: 83, D: 68, SPACE: 32, LK: 37, RK: 39, UK: 38, DK: 40};
 
@@ -184,19 +109,19 @@ function keyPressed(e) {
 		switch(e.keyCode) {
 			case KEY.W:
 			case KEY.UK:
-                player.y -= 5;
+                player.y -= 1;
 				break;
 			case KEY.A:
 			case KEY.LK:
-				player.x -= 5;
+				player.x -= 1;
 				break;
 			case KEY.S:
 			case KEY.DK:
-				player.y += 5;
+				player.y += 1;
 				break;
             case KEY.D:
 			case KEY.RK:
-				player.x += 5;
+				player.x += 1;
 				break;
             }
 }
@@ -206,15 +131,13 @@ function keyPressed(e) {
 Player
 ----------------------------------------*/
 
-var player = {x: 0, y: 0, orientation: 1, tileSize: 32, map: alpha, backpack: [], hp: 100, money: 0}
-
+var player = {x: 5, y: 5, orientation: 0, tileSize: 32, map: map, backpack: [], hp: 100, money: 0}
 
 /* --------------------------------------
 Movement 
 ----------------------------------------*/
 
 var movementTiles = [0, 6, 7, 8, 9, 10, 30, 31, 32];
-
 
 /* --------------------------------------
 Print functions 
