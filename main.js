598587239d52cb872613dca4a24c5982b28fab22
hlong16 <hlong17@Informations-MacBook-Air.local>
2017-05-23 21:37:21 -0400

    update from player branch

--- main.js
+++ main.js
@@ -24,17 +24,6 @@ var PokeMart = 64;
 
 var map = {cols: 16, rows: 16, tileSize: 16,
     tiles: [
-<<<<<<< HEAD
-        [1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 1, 1, 1],
-        [1, 2, 2, 2, 4, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
-        [1, 3, 3, 3, 4, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
-        [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
-=======
-        [1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 1, 1, 1, 1],
-        [1, 2, 2, 2, 4, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
-        [1, 3, 3, 3, 4, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
-        [1, 0, 0, 0, 0, 0, 0, 0, 0, 30, 30, 0, 0, 0, 0, 1],
->>>>>>> player
         [1, 0, 30, 30, 30, 30, 30, 30, 30, 30, 30, 0, 0, 0, 0, 1],
         [1, 0, 30, 30, 30, 30, 30, 30, 30, 30, 30, 0, 0, 0, 0, 1],
         [1, 0, 30, 30, 4, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 1],
@@ -75,12 +64,6 @@ var map = {cols: 16, rows: 16, tileSize: 16,
     }
 };
 
-<<<<<<< HEAD
-<<<<<<< HEAD
-
-=======
->>>>>>> master
-=======
 var beta = {cols: 16, rows: 16, tileSize: 16,
     tiles: [
     	[1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
@@ -126,7 +109,7 @@ var beta = {cols: 16, rows: 16, tileSize: 16,
     }
 };
 
->>>>>>> player
+
 window.onload = function() {
     canvas = document.getElementById("game");
     ctx = canvas.getContext("2d");
@@ -147,9 +130,7 @@ function drawMap() {
     for (var r = 0; r < player.map.rows; r++) {
         for (var c = 0; c < player.map.cols; c++) {
             var tile = player.map.getTile(r, c);
-            
             if (tile != -1) { // a value of -1 in the map array means empty tile
-
                     ctx.drawImage(
                         texture,            // image file source
                         0, // the x coordinate of the image file to clip --> texture file tile's x position
@@ -203,30 +184,10 @@ function drawMap() {
 }
 
 function drawPlayer() {
-<<<<<<< HEAD
-<<<<<<< HEAD
-    ctx.drawImage(
-        playerImg,
-        0,
-        0,
-        map.tileSize,
-        map.tileSize,
-        player.x * map.tileSize,
-        player.y * map.tileSize,
-        map.tileSize,
-        map.tileSize
-    );
-=======
-        ctx.drawImage(
-            playerImg, // image file source
-            0, 
-            player.orientation * player.tileSize,
-=======
         ctx.drawImage(
             playerImg, // image file source
             0, 
             (player.orientation - 1) * player.tileSize,
->>>>>>> player
             player.tileSize,
             player.tileSize,
             player.x * player.tileSize,
@@ -234,10 +195,6 @@ function drawPlayer() {
             map.tileSize * 2,
             map.tileSize * 2
             );
-<<<<<<< HEAD
->>>>>>> master
-=======
->>>>>>> player
 }
 
 //
@@ -250,10 +207,6 @@ function keyPressed(e) {
 		switch(e.keyCode) {
 			case KEY.W:
 			case KEY.UK:
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
->>>>>>> player
                 if (moveCheck(1)) {
                     player.y -= 1;
                     player.orientation = 1;
@@ -279,25 +232,6 @@ function keyPressed(e) {
 				    player.x += 1;
 				    player.orientation = 2;
                 }
-<<<<<<< HEAD
-=======
-                player.y -= 1;
-				break;
-			case KEY.A:
-			case KEY.LK:
-				player.x -= 1;
-				break;
-			case KEY.S:
-			case KEY.DK:
-				player.y += 1;
-				break;
-            case KEY.D:
-			case KEY.RK:
-				player.x += 1;
->>>>>>> master
-=======
->>>>>>> player
-				break;
             }
 }
 
@@ -306,15 +240,8 @@ function keyPressed(e) {
 Player
 ----------------------------------------*/
 
-<<<<<<< HEAD
-<<<<<<< HEAD
-var player = {x: 5, y: 5, orientation: 0, tileSize: 32, map: alpha1, backpack: [], hp: 100, money: 0};
-=======
-var player = {x: 5, y: 5, orientation: 0, tileSize: 32, map: map, backpack: [], hp: 100, money: 0}
->>>>>>> master
-=======
-var player = {x: 5, y: 5, orientation: 1, tileSize: 32, map: beta, backpack: [], hp: 100, money: 0}
->>>>>>> player
+var player = {x: 5, y: 5, orientation: 1, tileSize: 32, map: map, backpack: [], hp: 100, money: 0}
+
 
 /* --------------------------------------
 Movement 
@@ -322,9 +249,6 @@ Movement
 
 var movementTiles = [0, 6, 7, 8, 9, 10, 30, 31, 32];
 
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
 /* --------------------------------------
 Print functions 
 ----------------------------------------*/
@@ -337,46 +261,34 @@ function print(string) {
     text.appendChild(output); //add paragraph to div
 }
 
->>>>>>> player
+
 function moveCheck(input) {
     var moveTile;
+    var moveTile2;
     switch (input) {
         case 1:
             moveTile = player.map.getTile(player.y - 1, player.x);
+            moveTile2 = player.map.getTileSecond(player.y - 1, player.x);
             break;
         case 2:
             moveTile = player.map.getTile(player.y, player.x - 1);
+            moveTile2 = player.map.getTileSecond(player.y, player.x - 1);
             break;
         case 3:
             moveTile = player.map.getTile(player.y + 1, player.x);
+            moveTile2 = player.map.getTileSecond(player.y + 1, player.x);
             break;
         case 4:
             moveTile = player.map.getTile(player.y, player.x + 1);
+            moveTile2 = player.map.getTileSecond(player.y, player.x + 1);
             break;
     }
     console.log(moveTile);
-    if (movementTiles.indexOf(moveTile) != -1) {
-        
+    if (movementTiles.indexOf(moveTile) != -1 && movementTiles.indexOf(moveTile2) != -1) {
         return true;
     } else {
         return false;
     }
 }
 
-<<<<<<< HEAD
-=======
-/* --------------------------------------
-Print functions 
-----------------------------------------*/
 
-function print(string) {
-    var output = document.createElement("p"); //create a paragrahp element to hold the computer's response to input
-    var text = document.getElementById("textDiv"); //get div element for the entered text by user
-
-    output.appendChild(document.createTextNode(string)); //append child to output paragraph
-    text.appendChild(output); //add paragraph to div
-}
->>>>>>> master
-=======
-
->>>>>>> player
